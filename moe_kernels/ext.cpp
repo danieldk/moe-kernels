e151e27a42298294f0224360bffa754636de613f
Daniël de Kok <me@danieldk.eu>
2024-09-04 08:19:38 +0000

    Fix `marlin_gemm_moe` declaration

--- moe_kernels/ext.cpp
+++ moe_kernels/ext.cpp
@@ -6,8 +6,7 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("topk_softmax", &topk_softmax,
         "Apply topk softmax to the gating outputs");
 
-  #ifndef USE_ROCM
-  m.def("marlin_gemm_moe", &marlin_gemm_moe,
-        "Marlin GEMM for MoE layers");
-  #endif
+#ifndef USE_ROCM
+  m.def("marlin_gemm_moe", &marlin_gemm_moe, "Marlin GEMM for MoE layers");
+#endif
 }
