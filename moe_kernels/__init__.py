89134c2a1b09a36715ccb32659ec8b187c993789
Daniël de Kok <me@danieldk.eu>
2024-09-10 11:55:09 +0000

    Sync with upstream changes

--- moe_kernels/__init__.py
+++ moe_kernels/__init__.py
@@ -8,12 +8,13 @@ if HAS_TRITON:
 
     from .fused_moe import (
         fused_experts,
-        fused_marlin_moe,
         fused_moe,
         fused_topk,
         get_config_file_name,
         grouped_topk,
     )
+
+    from .fused_marlin_moe import fused_marlin_moe
 
     __all__ += [
         "fused_marlin_moe",
