7ef9c3f234f8b7e8ff1a753afd5d3fb4b89ac235
Daniël de Kok <me@danieldk.eu>
2024-09-23 09:22:44 +0000

    Sync with upstream to get support for 8-bit Marlin MoE

--- moe_kernels/fused_moe.py
+++ moe_kernels/fused_moe.py
@@ -9,7 +9,6 @@ import torch
 import triton
 import triton.language as tl
 
-import moe_kernels._ops as ops
 from .platforms import get_device_name
 from .fp8 import scaled_fp8_quant
 
@@ -225,7 +224,7 @@ def moe_align_block_size(
         (max_num_m_blocks,), dtype=torch.int32, device=topk_ids.device
     )
     num_tokens_post_pad = torch.empty((1), dtype=torch.int32, device=topk_ids.device)
-    ops.moe_align_block_size(
+    torch.ops._moe_kernels_ops.moe_align_block_size(
         topk_ids, num_experts, block_size, sorted_ids, expert_ids, num_tokens_post_pad
     )
     return sorted_ids, expert_ids, num_tokens_post_pad
@@ -402,7 +401,7 @@ def fused_topk(
         M, topk, dtype=torch.int32, device=hidden_states.device
     )
 
-    ops.topk_softmax(
+    torch.ops._moe_kernels_ops.topk_softmax(
         topk_weights,
         topk_ids,
         token_expert_indicies,
@@ -412,6 +411,7 @@ def fused_topk(
 
     if renormalize:
         topk_weights = topk_weights / topk_weights.sum(dim=-1, keepdim=True)
+
     return topk_weights, topk_ids
 
 
@@ -446,7 +446,8 @@ def grouped_topk(
 
     if renormalize:
         topk_weights = topk_weights / topk_weights.sum(dim=-1, keepdim=True)
-    return topk_weights, topk_ids
+
+    return topk_weights.to(torch.float32), topk_ids.to(torch.int32)
 
 
 def get_config_dtype_str(
@@ -581,7 +582,7 @@ def fused_experts(
             use_int8_w8a16=use_int8_w8a16,
         )
 
-        ops.silu_and_mul(intermediate_cache2, intermediate_cache1.view(-1, N))
+        torch.ops._moe_kernels_ops.silu_and_mul(intermediate_cache2, intermediate_cache1.view(-1, N))
 
         invoke_fused_moe_kernel(
             intermediate_cache2,
